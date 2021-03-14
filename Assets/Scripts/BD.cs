34ad0f94887bf6f7ff559711dba730291eb450d0
Andrey <andrey-199213@mail.ru>
2021-03-14 13:01:54 +0300

    v0.0.1
    настроено движение
    начало настройки анимации

---
+++ Assets/Scripts/BD.cs
@@ -0,0 +1,25 @@
+﻿
+public static class AxesNames
+{
+    public static string Horizontal = "Horizontal";
+    public static string Vertical = "Vertical";
+}
+
+public static class TagsNames
+{
+    public const string Player = "Player";
+    public const string Enemy = "Enemy";
+    public const string Bonus = "Bonus";
+}
+
+public static class MixerGroup
+{
+    public static string MusicVolume = "Volume Music";
+    public static string MasterVolume = "Volume Master";
+    public static string EffectsVolume = "Volume Effects";
+}
+
+public static class AnimParam
+{
+    public static string Speed = "Speed";
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField, Range(0,10)] float speed;
+
+    Rigidbody2D _rb;
+
+    #region Awake Start Update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+        Rotate();        
+    }
+    #endregion
+
+    #region Move & Rotate
+    void Move()
+    {
+        float inputX = Input.GetAxis(AxesNames.Horizontal);
+        float inputY = Input.GetAxis(AxesNames.Vertical);
+
+        _rb.velocity = new Vector2(inputX, inputY).normalized * speed;
+    }
+
+    void Rotate()
+    {
+        Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mouseWorldPosition - (Vector2)transform.position;
+
+        transform.up = direction;
+    }
+    #endregion
+}
